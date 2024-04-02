ab77d6ae780b7146375768843387d6bae2958a5c
Rhiannon Journey <rhiannonjourney@gmail.com>
2024-04-01 22:05:57 -0400

    Add ResourcePickerRenderHook class
    A new class named ResourcePickerRenderHook has been introduced. This class contains a constants that can be used to reference specific render hooks.

---
+++ src/Support/ResourcePickerRenderHook.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace UnexpectedJourney\FilamentResourcePicker\Support;
+
+class ResourcePickerRenderHook
+{
+    const RESOURCE_BROWSER_TABLE_BEFORE = 'resource-picker::resource-browser.table.before';
+}
